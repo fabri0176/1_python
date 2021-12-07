84f768bac4bcdb6c0f58a642a92643a930e5f9f3
Nestor Parra <fabri0176@gmail.com>
2021-12-07 08:35:48 -0500

    Creación clase aritmetica, se agregan metodos sumar, restar, dividir

--- 07_Clases_Objectos/02_Artimetica/Aritmetica.py
+++ 07_Clases_Objectos/02_Artimetica/Aritmetica.py
@@ -8,6 +8,26 @@ class Aritmetica:
     def sumar(self) -> int:
         return self.operandoA + self.operandoB
 
+    def restar(self) -> int:
+        return self.operandoA - self.operandoB
+
+    def dividir(self) -> float:
+        return self.operandoA / self.operandoB
+
+    def mostrarSuma(self) -> None:
+        print(f'La suma es {self.sumar()}')
+
+    def mostrarResta(self) -> None:
+        print(f'La resta es {self.restar()}')
+
+    def mostrarDivision(self) -> None:
+        print(f'El resultado de la división es {self.dividir():.2f}')
+
 
 aritmetica = Aritmetica(5, 4)
-print(aritmetica.sumar())
+aritmetica.mostrarResta()
+aritmetica.mostrarSuma()
+aritmetica.mostrarDivision()
+
+
+print(aritmetica.restar())
