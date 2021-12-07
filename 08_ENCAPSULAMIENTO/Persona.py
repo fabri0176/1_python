e9b40765196972985e71c7d9d7b3ca235d970628
Nestor Parra <fabri0176@gmail.com>
2021-12-07 09:13:03 -0500

    Encapsulamiento atributos clase persona, nombres, apellidos, num_identificación

--- 08_ENCAPSULAMIENTO/Persona.py
+++ 08_ENCAPSULAMIENTO/Persona.py
@@ -11,12 +11,28 @@ class Persona:
     @property
     def apellidos(self):
         return self._apellidos
-    
+
     @property
     def num_identificacion(self):
         return self._num_identificacion
 
+    @nombres.setter
+    def nombres(self, nombres):
+        self._nombres = nombres
+
+    @apellidos.setter
+    def apellidos(self, apellidos):
+        self._apellidos = apellidos
+
+    @num_identificacion.setter
+    def num_identificacion(self, num_identificacion):
+        self._num_identificacion = num_identificacion
+
 
 persona1 = Persona('Nestor Fabricio', 'Parra Gonzalez', 123213123)
 
-print(f'Soy {persona1.nombres} {persona1.apellidos}')
+print(f'Soy {persona1.nombres} {persona1.apellidos} CC {persona1.num_identificacion}')
+
+persona1.num_identificacion = 21313123213
+
+print(f'Soy {persona1.nombres} {persona1.apellidos} CC {persona1.num_identificacion}')
