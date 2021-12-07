b46accb04c2d868dea727ca134f6c14a8ad7a0b0
Nestor Parra <fabri0176@gmail.com>
2021-12-07 13:56:34 -0500

    Configuración clase abstracta

---
+++ 11_CLASES_ABSTRACTAS/FiguraGeometrica.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class FiguraGeometrica(ABC):
+
+    def __init__(self, ancho, alto) -> None:
+        self._ancho = ancho
+        self._alto = alto
+
+    @property
+    def ancho(self):
+        return self._ancho
+
+    @property
+    def alto(self):
+        return self._alto
+
+    @ancho.setter
+    def ancho(self, ancho):
+        self._ancho = ancho
+
+    @alto.setter
+    def alto(self, alto):
+        self._alto = alto
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
